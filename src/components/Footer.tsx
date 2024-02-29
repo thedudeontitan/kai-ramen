bee4534085dcf1521c21bd6f0a689e1a48860dc7
Ankit Kokane <ankitkokane90@gmail.com>
2024-02-29 17:15:57 +0530

    minor updates for mobile responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { FaFacebookF, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaInstagram,
+  FaRegCopyright,
+  FaTwitter,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="bg-[#333F72] bottom-0 left-0 right-0 z-20 text-white p-8 flex flex-row justify-between">
+    <div className="bg-[#333F72] bottom-0 left-0 right-0 z-20 text-white p-8 flex lg:flex-row flex-col justify-between gap-5">
       <p>Kai Ramen</p>
       <div className="flex flex-row gap-16 text-sm font-sans">
         <p className="font-hiro-misake">Home</p>
@@ -21,7 +27,7 @@ export default function Footer() {
           <Link to="">Team</Link>
         </div>
       </div>
-      <div className="flex flex-row gap-2">
+      <div className="flex flex-row gap-x-2 ">
         <Link to="" className="rounded-full border h-fit w-fit p-2">
           <FaTwitter />
         </Link>
@@ -35,6 +41,10 @@ export default function Footer() {
           <FaGithub />
         </Link>
       </div>
+      <p className="font-sans lg:absolute lg:bottom-0 lg:p-5 lg:right-0 flex flex-row items-center">
+        <FaRegCopyright className="pr-1 text-xl" />
+        Developed By Ankit Kokane
+      </p>
     </div>
   );
 }
--- src/pages/Specialities.tsx
+++ src/pages/Specialities.tsx
@@ -27,7 +27,7 @@ export default function Specialities() {
   return (
     <div className="flex flex-col items-center pb-10">
       <p className="text-5xl mt-14 mb-10">Our Specialties</p>
-      <div className="flex flex-row gap-10">
+      <div className="flex lg:flex-row flex-col gap-10">
         {items.map((i) => (
             <SepcialtyItem name={i.name} image={i.image} variety={i.variety}/>
         ))}
