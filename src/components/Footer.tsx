abd61bd251e50803bfde306367f09ea303f4051c
Ankit Kokane <ankitkokane90@gmail.com>
2024-02-29 16:55:47 +0530

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaFacebookF, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <div className="bg-[#333F72] bottom-0 left-0 right-0 z-20 text-white p-8 flex flex-row justify-between">
+      <p>Kai Ramen</p>
+      <div className="flex flex-row gap-16 text-sm font-sans">
+        <p className="font-hiro-misake">Home</p>
+        <div className="flex flex-col gap-2">
+          <p className="font-hiro-misake">Menu</p>
+          <Link to="">Sushi</Link>
+          <Link to="">Ramen</Link>
+          <Link to="">Mochi</Link>
+          <Link to="">Onigiri</Link>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-hiro-misake">About</p>
+          <Link to="">Company</Link>
+          <Link to="">Team</Link>
+        </div>
+      </div>
+      <div className="flex flex-row gap-2">
+        <Link to="" className="rounded-full border h-fit w-fit p-2">
+          <FaTwitter />
+        </Link>
+        <Link to="" className="rounded-full border h-fit w-fit p-2">
+          <FaFacebookF />
+        </Link>
+        <Link to="" className="rounded-full border h-fit w-fit p-2">
+          <FaInstagram />
+        </Link>
+        <Link to="" className="rounded-full border h-fit w-fit p-2">
+          <FaGithub />
+        </Link>
+      </div>
+    </div>
+  );
+}
