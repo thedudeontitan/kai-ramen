d92d3c815f60aa3656011c2479123be243d81015
Ankit Kokane <ankitkokane90@gmail.com>
2024-03-02 13:18:44 +0530

    added mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,51 @@
 import "../App.css";
-import { CiShoppingBasket } from "react-icons/ci";
+import { CiCircleInfo, CiShoppingBasket } from "react-icons/ci";
 import { FiUser } from "react-icons/fi";
+import { GiKnifeFork } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="z-20 fixed flex flex-row justify-between top-0 left-0 right-0 bg-[#F3EDE1] p-8 px-16 border-b border-[#333F72] border-opacity-30">
-      <div>
-        <Link to="/">Kai Ramen</Link>
-      </div>
-      <ul className="flex flex-row gap-14">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <a href="/home#menu">Menu</a>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <div className="flex flex-row gap-10 text-2xl">
+    <div>
+      <nav className="hidden z-20 fixed lg:flex flex-row justify-between top-0 left-0 right-0 bg-[#F3EDE1] p-8 px-16 border-b border-[#333F72] border-opacity-30">
         <div>
-          <CiShoppingBasket />
+          <Link to="/">Kai Ramen</Link>
         </div>
-        <div>
-          <FiUser />
+        <ul className="flex flex-row gap-14">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="/home#menu">Menu</a>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <div className="flex flex-row gap-10 text-2xl">
+          <div>
+            <CiShoppingBasket/>
+          </div>
+          <div>
+            <FiUser/>
+          </div>
         </div>
-      </div>
-      <div className="paperOverlay"></div>
+        <div className="paperOverlay"></div>
+      </nav>
+      <nav className="lg:hidden">
+        <div className="fixed top-0 left-0 right-0 h-20 bg-[#F3EDE1] py-8 border-b border-[#333F72] border-opacity-30 z-10">
+          <Link to="/" className="text-xl">Kai Ramen</Link>
 
-    </nav>
+          <div className="paperOverlay"></div>
+        </div>
+        <div className="flex flex-row justify-around fixed bottom-0 left-0 right-0 h-20 bg-[#F3EDE1] p-8 border-t border-[#333F72] border-opacity-30 z-10 text-3xl">
+          <Link to="/home#menu" className=""><GiKnifeFork/></Link>
+          <Link to="/" className=""><CiShoppingBasket/></Link>
+          <Link to="/about" className=""><CiCircleInfo/></Link>
+
+          <div className="paperOverlay"></div>
+        </div>
+      </nav>
+    </div>
   );
 }
