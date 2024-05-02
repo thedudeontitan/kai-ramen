2f398a56378067b34a7bba892c51b9a7c32be5d6
Ankit Kokane <ankitkokane90@gmail.com>
2024-05-02 15:55:18 +0530

    fixed cors issue

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(new URL('.', import.meta.url).pathname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
